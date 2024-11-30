733552e2a4832a28251e373544cca76cf0a84456
Michal Rygorowicz <mitsugi23@hotmail.com>
2024-11-30 12:43:46 +0100

    chore: add variables to globals and tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,80 @@ export default {
   ],
   theme: {
     extend: {
+      spacing: {
+        xxs: "var(--spacing-xxs)",
+        xs: "var(--spacing-xs)",
+        md: "var(--spacing-md)",
+        lg: "var(--spacing-lg)",
+        xl: "var(--spacing-xl)",
+        "2xl": "var(--spacing-2xl)",
+        "3xl": "var(--spacing-3xl)",
+      },
+
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        bg: {
+          primary: "var(--bg-primary)",
+          secondary: "var(--bg-secondary)",
+        },
+
+        border: {
+          primary: "var(--border-primary)",
+          secondary: "var(--border-secondary)",
+        },
+
+        button: {
+          primary: {
+            fg: "var(--button-primary-fg)",
+            bg: "var(--button-primary-bg)",
+            border: "var(--button-primary-border)",
+          },
+        },
+      },
+
+      borderRadius: {
+        md: "var(--radius-md)",
+      },
+
+      fontSize: {
+        "sm/regular": [
+          "var(--font-size-sm)",
+          {
+            lineHeight: "var(--line-height-sm)",
+            fontWeight: "var(--font-weight-regular)",
+          },
+        ],
+        "sm/medium": [
+          "var(--font-size-sm)",
+          {
+            lineHeight: "var(--line-height-sm)",
+            fontWeight: "var(--font-weight-medium)",
+          },
+        ],
+        "sm/semibold": [
+          "var(--font-size-sm)",
+          {
+            lineHeight: "var(--line-height-sm)",
+            fontWeight: "var(--font-weight-semibold)",
+          },
+        ],
+        "md/semibold": [
+          "var(--font-size-md)",
+          {
+            lineHeight: "var(--line-height-md)",
+            fontWeight: "var(--font-weight-semibold)",
+          },
+        ],
+        "md/regular": [
+          "var(--font-size-md)",
+          {
+            lineHeight: "var(--line-height-md)",
+            fontWeight: "var(--font-weight-regular)",
+          },
+        ],
       },
     },
   },
-  plugins: [],
 } satisfies Config;
